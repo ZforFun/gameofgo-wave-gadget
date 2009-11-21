8b63fb544082c577df63ae186b0bcafbe273ef0e
szabo.m.peter <szabo.m.peter@fad5f5ca-d545-11de-96bf-fbce4fd84a6e>
2009-11-21 00:27:00 +0000

    Surrounded stones are removed now automatically

--- trunk/js.js
+++ trunk/js.js
@@ -1,203 +1,413 @@
-
+// ----------------------------------------------------------
 // ----- Class: Game ----------------------------------------
-function Game(boardImageUrl, blackStoneImageUrl, whiteStoneImageUrl, boardSize, boardGeometry) {
-	this.boardImageUrl = boardImageUrl ;
-	this.boardImage = document.createElement("IMG") ;
-	this.boardImage.src = this.boardImageUrl ;
-	
-	this.blackStoneImageUrl = blackStoneImageUrl ;
-	this.whiteStoneImageUrl = whiteStoneImageUrl ;
-	
-	this.boardSize = boardSize ;
-	this.boardGeometry = boardGeometry ;
-	
-	console.debug(this.boardLeftOffset);
+// ----------------------------------------------------------
 
-	this.gameBoard = new GameBoard(boardSize) ;
-	this.gameLog   = new GameLog() ;
+function Game(boardImageUrl, blackStoneImageUrl, whiteStoneImageUrl, boardSize, boardGeometry) {
+    this.boardImageUrl = boardImageUrl ;
+    this.boardImage = document.createElement("IMG") ;
+    this.boardImage.src = this.boardImageUrl ;
+    
+    this.blackStoneImageUrl = blackStoneImageUrl ;
+    this.whiteStoneImageUrl = whiteStoneImageUrl ;
+    
+    this.boardSize = boardSize ;
+    this.boardGeometry = boardGeometry ;
+    
+    this.gameBoard = new GameBoard(boardSize) ;
+    this.gameLog   = new GameLog() ;
 }
 
 Game.prototype.resetBoard = function(div) {
-	var i, j ;
-	
-	// Remove stones, and old background...
-	if(this.div && this.stoneImages && this.stoneImages.length>0) {
-		for(i=0; i<this.stoneImages.length; i++) {
-			this.div.removeChild(this.stoneImages[i]) ;
-		}
-		this.div.removeChild(this.boardImage) ;
-	}
-	
-	// Save new div, add background
-	this.div = div ;
-	div.appendChild(this.boardImage) ;
-	
-	// Add stones
-	this.stoneImages = Array() ;
+    var i, j ;
+    
+    // Remove stones, and old background...
+    if(this.div && this.stoneImages && this.stoneImages.length>0) {
+        for(i=0; i<this.stoneImages.length; i++) {
+            this.div.removeChild(this.stoneImages[i]) ;
+        }
+        this.div.removeChild(this.boardImage) ;
+    }
+    
+    // Save new div, add background
+    this.div = div ;
+    div.appendChild(this.boardImage) ;
+    
+    // Add stones
+    this.stoneImages = Array() ;
 
-	for(i=0; i<this.boardSize; i++) {
-		for(j=0; j<this.boardSize; j++) {
-			var im = document.createElement("IMG") ;
-			im.src = this.blackStoneImageUrl ;
-			var x, y ;
-			x = Math.round(this.boardGeometry.getXforIndex(i) - im.width/2) ;
-			y = Math.round(this.boardGeometry.getYforIndex(j) - im.height/2) ;
-			
-			this.stoneImages[i*this.boardSize+j] = im ;
-			div.appendChild(im) ;
-			im.style.position = "absolute" ;
-			im.style.visibility = "hidden" ;
-			im.style.left = x+"px" ;
-			im.style.top = y+"px" ;
-			
-			// console.debug("Adding: "+i+", "+j+" -> "+im.style.left+", "+im.style.top) ;
-		}
-	}
-	
+    for(i=0; i<this.boardSize; i++) {
+        for(j=0; j<this.boardSize; j++) {
+            var im = document.createElement("IMG") ;
+            im.src = this.blackStoneImageUrl ;
+            var x, y ;
+            x = Math.round(this.boardGeometry.getXforIndex(i) - im.width/2) ;
+            y = Math.round(this.boardGeometry.getYforIndex(j) - im.height/2) ;
+            
+            this.stoneImages[i*this.boardSize+j] = im ;
+            div.appendChild(im) ;
+            im.style.position = "absolute" ;
+            im.style.visibility = "hidden" ;
+            im.style.left = x+"px" ;
+            im.style.top = y+"px" ;
+            
+            // console.debug("Adding: "+i+", "+j+" -> "+im.style.left+", "+im.style.top) ;
+        }
+    }
+    
 }
 
 Game.prototype.registerOnClick = function() {
-	var self = this ;
-	this.boardImage.onclick = function (event) {
-		self.onClickOnBoard(event) ;
-	}
+    var self = this ;
+    this.boardImage.onclick = function (event) {
+        self.onClickOnBoard(event) ;
+    }
 }
 
 Game.prototype.renderBoard = function() {
-	var i, j ;
-	for(i=0 ; i<this.boardSize; i++) {
-		for(j=0; j<this.boardSize; j++) {
-			var color = this.gameBoard.getField(i, j) ;
+    var i, j ;
+    for(i=0 ; i<this.boardSize; i++) {
+        for(j=0; j<this.boardSize; j++) {
+            var color = this.gameBoard.getField(i, j) ;
 
-			if(color) {
-				this.setStone(i, j, color) ;
-			}
-		}
-	}
+            this.setStone(i, j, color) ;
+        }
+    }
 }
 
 Game.prototype.setStone = function(i, j, color) {
-	var x, y ;
-	var visibility ;
-	var im = this.stoneImages[i*this.boardSize+j] ;
-	
-	if(color) {
-		if(color == 1) {
-			im.src = this.blackStoneImageUrl ;
-		} else {
-			im.src = this.whiteStoneImageUrl ;
-		}
-		visibility = "visible" ;
-	} else {
-		visibility = "hidden" ;
-	}
-	im.style.visibility = visibility ;
+    var x, y ;
+    var visibility ;
+    var im = this.stoneImages[i*this.boardSize+j] ;
+    
+    if(color) {
+        if(color == 1) {
+            im.src = this.blackStoneImageUrl ;
+        } else {
+            im.src = this.whiteStoneImageUrl ;
+        }
+        visibility = "visible" ;
+    } else {
+        visibility = "hidden" ;
+    }
+    im.style.visibility = visibility ;
 }
 
 Game.prototype.onClickOnBoard = function(event) {
-	var x, y ;
-	
-	if(event.offsetX) {
-		x = event.offsetX ;
-	} else {
-		x = event.pageX - this.div.offsetLeft ;
-	}
-	
-	if(event.offsetY) {
-		y = event.offsetY ;
-	} else {
-		y = event.pageY - this.div.offsetTop ;
-	}
-	
-	var i, j ;
-	
-	i = this.boardGeometry.getIndexForX(x) ;
-	j = this.boardGeometry.getIndexForY(y) ;
+    var x, y ;
+    
+    if(event.offsetX) {
+        x = event.offsetX ;
+    } else {
+        x = event.pageX - this.div.offsetLeft ;
+    }
+    
+    if(event.offsetY) {
+        y = event.offsetY ;
+    } else {
+        y = event.pageY - this.div.offsetTop ;
+    }
+    
+    var i, j ;
+    
+    i = this.boardGeometry.getIndexForX(x) ;
+    j = this.boardGeometry.getIndexForY(y) ;
 
-	var color = 1 ;
-	var step = this.gameLog.lastStep() 
-	if(step && step[2]==color) color = 2 ;
-
-	this.gameLog.addStep(i, j, color) ;
-	this.gameBoard.applyLog(this.gameLog) ;
-	this.renderBoard() ;
+    this.gameBoard.setMove(i, j) ;
+    this.renderBoard() ;
 }
 
 
+// ----------------------------------------------------------
+// ----- Class: GameLog -------------------------------------
+// ----------------------------------------------------------
+
 function GameLog() {
-	this.log = new Array() ;
+    this.log = new Array() ;
 }
 
 GameLog.prototype.addStep = function(i, j, color) {
-	this.log[this.log.length] = new Array(i, j, color) ;
+    this.log[this.log.length] = new GameBoardStone(i, j, color) ;
 }
 
 GameLog.prototype.getStep = function(index) {
-	return this.log[index] ;
+    return this.log[index] ;
 }
 
 GameLog.prototype.lastStep = function() {
-	var rv = null ;
-	if(this.log.length>0) {
-		rv = this.log[this.log.length-1] ;
-	}
-	return rv ;
+    var rv = null ;
+    if(this.log.length>0) {
+        rv = this.log[this.log.length-1] ;
+    }
+    return rv ;
 }
 
 GameLog.prototype.getLength = function() {
-	return this.log.length ;
+    return this.log.length ;
 }
 
-function GameBoard(size) {
-	this.board = Array() ;
-	this.size = size ;
+function GameBoardStone(x, y, color) {
+    this.x = x ;
+    this.y = y ;
+    this.color = color ;
+}
+
+function gameBoardStoneComparator(o1, o2) {
+    if(o1.x<o2.x) return -1 ;
+    if(o1.x>o2.x) return  1 ;
+    if(o1.y<o2.y) return -1 ;
+    if(o1.y>o2.y) return  1 ;
+
+    return 0 ;
+}
+
+// ----------------------------------------------------------
+// ----- Class: GameBoard -----------------------------------
+// ----------------------------------------------------------
+
+function GameBoard(boardSize) {
+    this.board = Array() ;
+    this.boardSize = boardSize ;
+    this.gameLog = new GameLog() ;
 }
 
 GameBoard.prototype.setMove = function (x, y, color) {
-	var field = this.board[x*this.size+y] ;
-	console.debug("setMove", x, y, color, field) ;
-	if(field) return false ;
-	
-	this.board[x*this.size+y] = color ;
-	return true ;
+    var field = this.board[x*this.boardSize+y] ;
+
+    // If field is occupied, this is an invalid move...
+    if(field) return false ;
+    
+    // Determine the other color and other color
+    if(color == null) {
+        color = 1 ;
+        var lastLogStep = this.gameLog.lastStep() ;
+        if(lastLogStep && lastLogStep.color == 1) {
+            color = 2 ;
+        }
+    }
+    
+    var otherColor = 1;
+    if(color == 1) otherColor = 2 ;
+    
+    // Check if this move removes anything:
+    var neighbours  = this.getNeighbours(x, y, otherColor) ;
+    var moveAllowed = false ;
+    
+    while(!neighbours.isEmpty()) {
+        var neighbour = neighbours.pop() ;
+        
+        if(neighbour.color != otherColor) continue ;
+        
+        var shape = new SortedSet(gameBoardStoneComparator) ;
+        var lives = new SortedSet(gameBoardStoneComparator) ;
+            
+        this.walkShape(neighbour.x, neighbour.y, shape, lives) ;
+        // There is a shape, and it has only one life (the one we are placing on)
+        if(shape.data.length > 0 && lives.data.length <=1) {
+            this.removeShape(shape) ;
+            moveAllowed = true ;
+        }
+    }
+    
+    this.board[x*this.boardSize+y] = color ;
+    
+    if(!moveAllowed) {
+        var shape = new SortedSet(gameBoardStoneComparator) ;
+        var lives = new SortedSet(gameBoardStoneComparator) ;
+        this.walkShape(x, y, shape, lives) ;
+        if(lives.data.length>=1) {
+            moveAllowed = true ;
+        }
+    }
+
+    if(!moveAllowed) {
+        this.board[x*this.boardSize+y] = null ;
+    } else {
+        this.gameLog.addStep(x, y, color) ;
+    }
+    
+    return moveAllowed ;
 }
 
+GameBoard.prototype.getNeighbours = function(x, y) {
+    var rv = new SortedSet(gameBoardStoneComparator) ;
+    
+    var xx = x-1
+    var yy = y ;
+    var stone = this.getField(xx, yy);
+    
+    if(xx>=0)        
+        rv.insert(new GameBoardStone(xx, yy, stone)) ;
+
+    var xx = x+1
+    var yy = y ;
+    var stone = this.getField(xx, yy);
+    
+    if(xx<this.boardSize)        
+        rv.insert(new GameBoardStone(xx, yy, stone)) ;
+
+    var xx = x
+    var yy = y-1 ;
+    var stone = this.getField(xx, yy);
+    
+    if(yy>=0)        
+        rv.insert(new GameBoardStone(xx, yy, stone)) ;
+
+    var xx = x
+    var yy = y+1 ;
+    var stone = this.getField(xx, yy);
+    
+    if(yy<this.boardSize)        
+        rv.insert(new GameBoardStone(xx, yy, stone)) ;
+        
+    return rv ;
+}
+
+GameBoard.prototype.walkShape = function(x, y, shape, lives) {
+    
+    // Only proceed if something is at x, y
+    var color = this.getField(x, y) ; 
+    if(color) {
+        var openPoints = new SortedSet(gameBoardStoneComparator) ;
+        
+        // seed the open points with the current point...
+        openPoints.insert(new GameBoardStone(x, y, color)) ;
+        
+        // while there are open points, try to close them...
+        while(!openPoints.isEmpty()) {
+            var openPoint = openPoints.pop() ;
+            var neighbours = this.getNeighbours(openPoint.x, openPoint.y) ;
+
+            while(!neighbours.isEmpty()) {
+                var neighbour = neighbours.pop() ;
+                
+                if(neighbour.color == color && !shape.contains(neighbour)) {
+                    openPoints.insert(neighbour) ;
+                }
+                
+                if(!neighbour.color && !lives.contains(neighbour)) {
+                    lives.insert(neighbour) ;
+                }
+            }
+            
+            shape.insert(openPoint) ;
+        }
+    }
+    
+    return shape ;
+}
+
+GameBoard.prototype.removeShape = function (shape) {
+    for(var i=0; i<shape.data.length; i++) {
+        var stone = shape.data[i] ;
+        this.board[stone.x*this.boardSize+stone.y] = null ;
+    }
+} 
+
 GameBoard.prototype.getField = function (x, y) {
-	return this.board[x*this.size+y] ;
+    return this.board[x*this.boardSize+y] ;
 }
 
 GameBoard.prototype.applyLog = function (log) {
-	var len = log.getLength() ;
-	var i ;
+    var len = log.getLength() ;
+    var i ;
 
-	for(i=0; i<len; i++) {
-		var s = log.getStep(i) ;
-		this.setMove(s[0], s[1], s[2]) ;
-	}
+    for(i=0; i<len; i++) {
+        var s = log.getStep(i) ;
+        this.setMove(s.x, s.y, s.color) ;
+    }
 }
 
-//-------------------------------------------------------------
+// ----------------------------------------------------------
+// ----- Class: GameBoardGeometry ---------------------------
+// ----------------------------------------------------------
+
 function GameBoardGeometry(firstX, incX, firstY, incY) {
-	this.firstX = firstX ;
-	this.incX = incX ;
-	this.firstY = firstY ;
-	this.incY = incY ;
+    this.firstX = firstX ;
+    this.incX = incX ;
+    this.firstY = firstY ;
+    this.incY = incY ;
 }
 
 GameBoardGeometry.prototype.getXforIndex = function(i) {
-	return i*this.incX+this.firstX ;
+    return i*this.incX+this.firstX ;
 }
 
 GameBoardGeometry.prototype.getYforIndex = function(j) {
-	return j*this.incY+this.firstY ;
+    return j*this.incY+this.firstY ;
 }
 
 GameBoardGeometry.prototype.getIndexForX = function(x) {
-	var rv = (x - this.firstX) + this.incX/2 ;
-	return Math.floor(rv/this.incX) ;
+    var rv = (x - this.firstX) + this.incX/2 ;
+    return Math.floor(rv/this.incX) ;
 }
 
 GameBoardGeometry.prototype.getIndexForY = function(y) {
-	var rv = (y - this.firstY) + this.incY/2 ;
-	return Math.floor(rv/this.incY) ;
+    var rv = (y - this.firstY) + this.incY/2 ;
+    return Math.floor(rv/this.incY) ;
+}
+
+// ----------------------------------------------------------
+// ----- Class: SordedSet -----------------------------------
+// ----------------------------------------------------------
+
+function SortedSet(cmp) {
+    this.comparator = cmp ;
+    this.data = new Array() ;
+}
+
+SortedSet.prototype.findPosition = function (e) {
+    if(this.data.length==0) return 0 ;
+    
+    var low = 0 ;
+    var high = this.data.length ;
+    var middle = Math.floor((low+high)/2) ;
+    
+    while(low<high) {
+        var cmp = this.comparator(e, this.data[middle]) ;
+        if(cmp==0) return middle ;
+        else if(cmp<0) high = middle ;
+        else low = middle+1;
+        middle = Math.floor((low+high)/2) ;
+    }
+    return high ;
+}
+
+SortedSet.prototype.contains = function(e) {
+    if(this.data.length == 0) return false ;
+    
+    var value = this.data[this.findPosition(e)] ;
+    return e && value && (this.comparator(value, e)==0) ;
+}
+
+SortedSet.prototype.insert = function(e) {
+    var pos = this.findPosition(e) ;
+    
+    // Already in set...
+    if(pos<this.data.length && this.comparator(this.data[pos], e)==0) {
+        return ;
+    }
+    
+    this.data.splice(pos, 0, e) ;
+}
+
+SortedSet.prototype.remove = function(e) {
+    var pos = this.findPosition(e) ;
+    
+    if(this.data.length>0 && this.comparator(this.data[pos], e)==0) {
+        this.data.splice(pos, 1) ;
+    }
+}
+
+SortedSet.prototype.insertAllElements = function(sm) {
+    for(var i=0; i<sm.data.length; i++) {
+        this.insert(sm.data[i]) ;
+    }
+}
+
+SortedSet.prototype.isEmpty = function() {
+    return this.data.length == 0;
+}
+
+SortedSet.prototype.pop = function() {
+    return this.data.pop() ;
 }
