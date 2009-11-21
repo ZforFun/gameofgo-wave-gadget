cf35b98a71fee1b2b8c08a12d7a838be0ffdda72
szabo.m.peter <szabo.m.peter@fad5f5ca-d545-11de-96bf-fbce4fd84a6e>
2009-11-21 13:57:47 +0000

    Added script to create the basic board with Image Magick: graphics re-created, stones look better

--- trunk/mkboard.sh
+++ trunk/mkboard.sh
@@ -1,14 +1,57 @@
-i=20
-while [ $i -lt 600 ]; do
+#!/bin/bash
+
+# Configure the size / filename parameters
+BOARD_LEFT_OFFSET=20
+BOARD_TOP_OFFSET=20
+BOARD_HORIZONTAL_GAP=32
+BOARD_VERTICAL_GAP=32
+BOARD_SIZE=19
+BOARD_FILENAME=board.png
+BOARD_BACKGROUND_COLOR="#EBCA70"
+
+STONE_SIZE=24
+BLACK_STONE_FILENAME=black.png
+BLACK_STONE_COLOR="#000000"
+WHITE_STONE_FILENAME=white.png
+WHITE_STINE_COLOR="#EBEBEB"
+
+########################
+
+
+# Create the stones
+STONE_IMAGE_SIZE="$STONE_SIZE"x"$STONE_SIZE"
+DRAW_COMMAND="circle $((STONE_SIZE / 2 - 1)),$((STONE_SIZE / 2 - 1)) $((STONE_SIZE / 2)),0"
+# DRAW_COMMAND="circle 0,0 $STONE_SIZE,$STONE_SIZE"
+convert  -size "$STONE_IMAGE_SIZE" xc:none -fill "$BLACK_STONE_COLOR" -draw "$DRAW_COMMAND"  \
+         "$BLACK_STONE_FILENAME"
+convert  -size "$STONE_IMAGE_SIZE" xc:none -fill "$WHITE_STONE_COLOR" -draw "$DRAW_COMMAND"  \
+         "$WHITE_STONE_FILENAME"
+
+# Create the board...
+i="$(($BOARD_LEFT_OFFSET * 2 + (BOARD_SIZE - 1) * BOARD_HORIZONTAL_GAP))"
+j="$((BOARD_TOP_OFFSET * 2 + (BOARD_SIZE-1) * BOARD_VERTICAL_GAP))"
+convert  -size "$i"x"$j" \
+         xc:none -fill "$BOARD_BACKGROUND_COLOR" -background "$BOARD_BACKGROUND_COLOR" \
+         -draw "rectangle 0,0 $i,$j" \
+         "$BOARD_FILENAME"
+         
+
+# Put on the lines...
+i="$BOARD_LEFT_OFFSET"
+j=$((BOARD_LEFT_OFFSET + (BOARD_SIZE-1) * BOARD_HORIZONTAL_GAP))
+while [ "$i" -le "$j" ]; do
   echo $i
-  /opt/local/bin/mogrify -draw "line 20, $i, 596, $i" tmp.png
-  i=$((i+32))
+  mogrify -draw "line $BOARD_LEFT_OFFSET, $i, $j, $i" "$BOARD_FILENAME"
+  i=$((i+BOARD_HORIZONTAL_GAP))
 done
 
-i=20
-while [ $i -lt 600 ]; do
+i="$BOARD_TOP_OFFSET"
+j=$((BOARD_TOP_OFFSET + (BOARD_SIZE-1) * BOARD_VERTICAL_GAP))
+while [ "$i" -le "$j" ]; do
   echo $i
-  /opt/local/bin/mogrify -draw "line $i, 20, $i, 596" tmp.png
-  i=$((i+32))
+  mogrify -draw "line $i, $BOARD_TOP_OFFSET, $i, $j" "$BOARD_FILENAME"
+  i=$((i+BOARD_VERTICAL_GAP))
 done
 
+# Draw the handicap points
+# TODO:
