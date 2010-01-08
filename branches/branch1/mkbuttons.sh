3624f821f6fe2ca3acacba48b5e7a931404b5ecb
bartmate <bartmate@fad5f5ca-d545-11de-96bf-fbce4fd84a6e>
2010-01-08 00:09:04 +0000

    Deleting

--- branches/branch1/mkbuttons.sh
+++
@@ -1,3 +0,0 @@
-convert -format png -pointsize 14 -gravity Center -annotate 0 "Undo" -size 60x30  xc:none undo.png
-convert -format png -pointsize 14 -gravity Center -annotate 0 "Redo" -size 60x30  xc:none redo.png
-convert -format png -pointsize 14 -gravity Center -annotate 0 "Pass" -size 60x30  xc:none pass.png
